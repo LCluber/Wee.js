2852565b1788d00e072669a1ca4ea0bbed434773
Ludovic CLUBER <ludovic.cluber@gmail.com>
2018-08-28 18:03:54 +0200

    deleted mouettejs

--- website/js/example.js
+++
@@ -1,7 +0,0 @@
-
-// var toto = WEE.Ajax.call('assets/shaders/shader1.txt')
-//                    .then(function (val) { console.log(val); })
-//                    .catch(function (err) { console.log('error', err.message); });
-
-// var exampleDiv = Wee.Dom.findById('exampleDiv');
-// Wee.Dom.addHTMLElement(exampleDiv, 'p', {content: 'Ce paragraphe a été ajouté dynamiquement.', class: 'text-primary'});
